584e9dcd47ebd53de18555751cbbe7176c4df302
Richard Iribarren <richardiribarren@gmail.com>
2024-02-17 03:42:42 -0400

    build: se agrega semantic-release

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-anallclearyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+  ],
+};
